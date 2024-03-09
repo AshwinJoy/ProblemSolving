7cdee87caeed61265cb80219cb6a4daa9d0083a5
Ashwin Joy <ashwinparekkudiyil@gmail.com>
2024-03-09 12:08:15 +0530

    Updated trappedwater.py with a better code

--- trappedwater.py
+++ trappedwater.py
@@ -1,16 +1,25 @@
-towers = list(map(int,input().split(",")))
-stack = []
-n = len(towers)
-water_trapped = 0
+def trap_water(heights):
+    if not heights:
+        return 0
 
-for i in range(n):
-    while( len(stack)!=0 and (towers[i] > towers[stack[-1]]) ):
-        popped_height = towers[stack[-1]]
-        stack.pop()
-        if len(stack) == 0:
-            break
-        distance = i - stack[-1] - 1
-        min_height = min(towers[stack[-1]],towers[i]) - popped_height
-        water_trapped += distance * min_height
-    stack.append(i)
-print(water_trapped)
+    n = len(heights)
+    water_trapped = 0
+    stack = []
+
+    for i in range(n):
+        while stack and heights[i] > heights[stack[-1]]:
+            top = stack.pop()
+
+            if not stack:
+                break
+
+            distance = i - stack[-1] - 1
+            bounded_height = min(heights[i], heights[stack[-1]]) - heights[top]
+            water_trapped += distance * bounded_height
+
+        stack.append(i)
+
+    return water_trapped
+
+heights = list(map(int, input().split(",")))
+print(trap_water(heights))
